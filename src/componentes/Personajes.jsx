40ccd6b19abde389da0e6ffcf01b0609262b7dd8
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-09-30 21:46:47 -0300

    Actualización Bootstrap

--- src/componentes/Personajes.jsx
+++ src/componentes/Personajes.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Personajes({ personaje }) {
-    return (
-        /*9. Cada elemento(personaje) contenido en este div, esta asociado a una
+  return (
+    /*9. Cada elemento(personaje) contenido en este div, esta asociado a una
         propiedad {name} o {image}*/
-        <div>
-            <h2>{personaje.name}</h2>
-            <img src={personaje.image} alt="personaje.nombre" />
-        </div>
-    );
+    <div className="text-center p-5">
+      <h3>{personaje.name}</h3>
+      <img
+        className="img-fluid rounded-pill border border-dark border-3"
+        src={personaje.image}
+        alt="personaje.nombre"
+      />
+      <p>{personaje.origin.name}</p>
+    </div>
+  );
 }
 
 export default Personajes;
