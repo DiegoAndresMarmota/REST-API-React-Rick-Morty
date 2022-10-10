d21afced33d80647c3abfbb052a58dd5eb1597e7
Diego Andres <diegoechalarp@hotmail.com>
2022-10-09 21:32:29 -0300

    useState del Navbar

--- src/componentes/ListaPersonajes.jsx
+++ src/componentes/ListaPersonajes.jsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react";
 import Personajes from "./Personajes";
 
-function NavegacionPagina() {
+function Pagina({ pagina, setPagina }) {
   return (
-    <div className="d-flex align-items-center justify-content-between">
-      <p className="pt-2">Página Actual: 1</p>
-      <button className="btn btn-success btn-sm">Página Siguiente</button>
+    <div
+      className="d-flex align-items-center justify-content-between">
+      <p
+        className="pt-2">
+        Página Actual: {pagina}
+      </p>
+      <button
+        className="btn btn-success btn-sm"
+        onClick={() => { setPagina(pagina + 1)}}>
+        Página: {pagina + 1}
+      </button>
     </div>
   );
 }
@@ -14,14 +22,14 @@ function ListaPersonajes() {
   /*6. Crear un useState */
   const [personajes, setPersonajes] = useState([]);
   const [cargando, setCargando] = useState(true);
+  /*A. Cuando carga la pagina, se carga en la pag. 1*/
+  const [pagina, setPagina] = useState(1);
 
   useEffect(() => {
     /*4. Se crea la f(datosFetch)*/
     async function datosFetch() {
       /*1.Se realiza la petición(asincrona con el await) al back-end del API de Rick and Morty*/
-      const esperarFetch = await fetch(
-        "https://rickandmortyapi.com/api/character"
-      );
+      const esperarFetch = await fetch(`https://rickandmortyapi.com/api/character?page=${pagina}`);
 
       /*2.Convertir los datos en un json y se almacena en un const*/
       const datosAlmacenados = await esperarFetch.json();
@@ -36,12 +44,17 @@ function ListaPersonajes() {
 
     /*5. Se llama la f(datosFetch)*/
     datosFetch();
-  }, []);
+    /*C. El useEffect pendiente de este valor [pagina]*/
+  }, [pagina]);
 
   return (
-    <div className="container bg-white text-dark border-2">
+    <div className="container text-white border-2">
 
-      <NavegacionPagina />
+      {/*B. Al dar un click en el componente Pagina, se actualiza la pagina actual y el boton con la pagina subsiguiente*/}
+      <Pagina
+        pagina={pagina}
+        setPagina={setPagina}
+      />
 
       {/*8.Voy a recorrer(map) cada elemento(personaje) de personajes, retornando sus datos en el div, siendo <h2> y <img>*/}
       {cargando ? (
